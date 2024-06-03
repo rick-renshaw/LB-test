bb9e16833812ddd22daf5b2737aa192e41fd0b98
Rick Renshaw <rick.renshaw@mpspark.com>
2024-06-03 10:24:39 -0500

    fix issues

--- data.tf
+++ data.tf
@@ -2,7 +2,7 @@ data "aws_ami" "ami" {
   most_recent = true
   filter {
     name   = "tag:Name"
-    values = ["al2023-ami-2023.*-kernel-6.1-x86_64"]
+    values = ["al2023-ami-2023.*-x86_64"]
   }
   owners = ["amazon"]
 }
