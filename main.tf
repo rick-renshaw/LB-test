a216161eaa35d3accd3b8267e536f2dd6ed01d98
Rick Renshaw <rick.renshaw@mpspark.com>
2024-06-03 10:42:41 -0500

    add ssh

--- main.tf
+++ main.tf
@@ -22,6 +22,24 @@ resource "aws_security_group" "http" {
   }
 }
 
+resource "aws_security_group" "ssh" {
+  vpc_id      = var.vpd_id
+  name        = "lb-test-allow-ssh"
+  description = "allow port 22"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "nginx" {
   ami                    = data.aws_ami.ami.id
   instance_type          = "t3.micro"
