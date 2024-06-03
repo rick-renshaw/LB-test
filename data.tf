448238e5f16e423b06c1b8ea42c8839eebfa9203
Rick Renshaw <rick.renshaw@mpspark.com>
2024-06-03 10:30:54 -0500

    fix issues

--- data.tf
+++ data.tf
@@ -1,8 +1,16 @@
 data "aws_ami" "ami" {
   most_recent = true
   filter {
-    name   = "tag:Name"
+    name   = "name"
     values = ["al2023-ami-2023.*-x86_64"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
   owners = ["amazon"]
 }
