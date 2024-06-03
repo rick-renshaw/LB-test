adfd87ea46a563daac4ea5a595ac5b8ce669bceb
Rick Renshaw <rick.renshaw@mpspark.com>
2024-06-03 11:05:03 -0500

    add tga

--- main.tf
+++ main.tf
@@ -79,3 +79,9 @@ resource "aws_lb_listener" "port_80" {
     target_group_arn = aws_lb_target_group.port_80.arn
   }
 }
+
+resource "aws_lb_target_group_attachment" "target" {
+  target_group_arn = aws_lb_target_group.port_80.arn
+  target_id        = aws_instance.nginx.id
+  port             = 80
+}
