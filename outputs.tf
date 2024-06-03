376138e673c1c679ccaa7c261708df41365a461d
Rick Renshaw <rick.renshaw@mpspark.com>
2024-06-03 11:30:50 -0500

    output ip

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "instance" {
   value = aws_instance.nginx.id
 }
 
+output "instance_ip" {
+  value = aws_instance.nginx.private_ip
+}
+
 output "lb_url" {
   value = aws_lb.lbtest.dns_name
 }
