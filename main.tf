3cb7588dfab59c14d6f62612458a2f6d5fbda025
Rick Renshaw <rick.renshaw@mpspark.com>
2024-06-11 14:53:07 -0500

    updated

--- main.tf
+++ main.tf
@@ -12,13 +12,15 @@ resource "aws_security_group" "http" {
     from_port   = 80
     to_port     = 80
     protocol    = "TCP"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = local.mps_cidrs
+    description = "Allow access to port 80 from our networks"
   }
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow everything out"
   }
 }
 
@@ -30,13 +32,15 @@ resource "aws_security_group" "ssh" {
     from_port   = 22
     to_port     = 22
     protocol    = "TCP"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = local.mps_cidrs
+    description = "Allow access to port 22 from our networks"
   }
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow everything out"
   }
 }
 
@@ -47,19 +51,34 @@ resource "aws_instance" "nginx" {
   vpc_security_group_ids = [aws_security_group.http.id, aws_security_group.ssh.id]
   key_name               = "prod-aws-nexus-11-04-2021"
   user_data_base64       = filebase64("${path.module}/nginx.sh")
+  monitoring             = true
+  ebs_optimized          = true
+  metadata_options {
+    http_tokens = "required"
+  }
+  root_block_device {
+    encrypted = true
+  }
   tags = {
     Name = "LB-test"
   }
 }
 
 resource "aws_lb" "lbtest" {
-  internal             = false
-  ip_address_type      = "ipv4"
-  load_balancer_type   = "application"
-  name                 = "test-load-balancer"
-  preserve_host_header = false
-  security_groups      = [aws_security_group.http.id]
-  subnets              = [var.subnet_id, var.subnet_b_id]
+  internal                   = false
+  ip_address_type            = "ipv4"
+  load_balancer_type         = "application"
+  name                       = "test-load-balancer"
+  preserve_host_header       = false
+  security_groups            = [aws_security_group.http.id]
+  subnets                    = [var.subnet_id, var.subnet_b_id]
+  enable_deletion_protection = true
+  drop_invalid_header_fields = true
+  access_logs {
+    bucket  = aws_s3_bucket.fib.bucket
+    prefix  = "test-lb"
+    enabled = true
+  }
 }
 
 resource "aws_lb_target_group" "port_80" {
@@ -67,7 +86,13 @@ resource "aws_lb_target_group" "port_80" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
-
+  health_check {
+    enabled           = true
+    healthy_threshold = 3
+    interval          = 30
+    matcher           = "200-299"
+    path              = "/"
+  }
 }
 
 resource "aws_lb_listener" "port_80" {
@@ -84,4 +109,16 @@ resource "aws_lb_target_group_attachment" "target" {
   target_group_arn = aws_lb_target_group.port_80.arn
   target_id        = aws_instance.nginx.id
   port             = 80
+}
+
+resource "aws_s3_bucket" "fib" {
+  bucket        = "mpspark-test-bucket"
+  force_destroy = true
+  tags = {
+    Name = "foo-bucket"
+  }
+  versioning {
+    enabled = true
+  }
+  
 }
